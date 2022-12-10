2d766dc06e8919054db0f53b071340b63ace60d0
AAAlila <angelaalila13@gmail.com>
2022-12-10 13:26:26 -0800

    alphABET

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ * main - Entry Point
+ *
  * Return: Always 0
  */
 int main(void)
@@ -8,7 +9,7 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-		putchar(a);
+		printf("%d", a);
 	putchar('\n');
 	return (0);
 }
