7e34f0c12be7039670c5d7bf29a0595aa3b052c3
AAAlila <angelaalila13@gmail.com>
2022-12-13 14:19:51 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry Point
- *
+ * Description: prints single number 
  * Return :Always 0
  */
-
 int main(void)
 {
 	int a;
-
-	for (a = 0; a < 10; a++)
+	
+	if (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
