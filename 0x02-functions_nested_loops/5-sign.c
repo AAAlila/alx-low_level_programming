bdf4592148421d38622d4a86aca4199bac6b2dbe
AAAlila <angelaalila13@gmail.com>
2022-12-14 13:01:46 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sighn of a number
+ * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
  * Return: 1 if the number is greater than zero
  * 0 of number is zero
