d91de8e22cd5be9d6b09c92769d5a2a6392703e9
AAAlila <angelaalila13@gmail.com>
2022-12-19 16:05:18 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * reset to 98 - update value of n to 98
+ * reset_to_98 - update value of n to 98
+ *
  * @n: input
+ *
  * Return: n
  */
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
